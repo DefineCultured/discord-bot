5401aefb921df66ddbd5ecfc132556852aa1b6d1
Jan Henning <triton539@hotmail.com>
2020-10-25 14:04:44 +0100

    update gitignore

--- dist/src/modules/events/error.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = (_bot, error) => {
-    console.error(error);
-    throw error;
-};
-//# sourceMappingURL=error.js.map
--- dist/src/modules/events/ready.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports = (bot) => {
-    console.log(`${bot.user.username} is online!`);
-    bot.user.setActivity('definecultured.com');
-};
-//# sourceMappingURL=ready.js.map
