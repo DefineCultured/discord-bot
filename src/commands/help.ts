b23d8734a9d7b7d8a924fde5ecbe5dde1ba51cd3
Jan Henning <triton539@hotmail.com>
2020-10-29 15:51:54 +0100

    help command embed

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -6,11 +6,11 @@ import { Bot } from '../interfaces'
 module.exports.run = async (_bot: Bot, message: Message, _args: any) => {
   const helpEmbed = new MessageEmbed()
     .setColor('#00FF00')
-    .setAuthor('Define Cultured Help', 'https://i.imgur.com/mVKllA1.jpg', 'https://definecultured.com/')
+    .setAuthor('Define Cultured', 'https://i.imgur.com/mVKllA1.jpg', 'https://definecultured.com/')
     .setThumbnail('https://i.imgur.com/mVKllA1.jpg')
     .addField('!clearcache', 'Clear current cache. (alias !cc)')
     .addField('!emails', 'Get current amount of emails. (alias !e)')
-    .addField('!export', 'Export current signups to CSV file.')
+    .addField('!export', 'Export current signup data to CSV file.')
     .addField('!help', 'Show this help embed. (alias !commands)')
     .setTimestamp()
     .setFooter(`Define Cultured Bot v${config.version}`)
